5856708e688702c178b03d5595fe5515068c3b7f
kiyo2700 <133238214+kiyo2700@users.noreply.github.com>
2023-05-11 23:16:58 +0900

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/onepiece01_luffy.png') {
+    myImage.setAttribute('src','images/onepiece14_enel.png');
+  } else {
+    myImage.setAttribute('src','images/onepiece01_luffy.png');
+  }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  const myName = prompt('あなたの名前を入力してください。');
+  if (!myName){
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla はかっこいいよ、${myName}`;
+  }
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem('name');
+  myHeading.textContent = `Mozilla はかっこいいよ、${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+}
